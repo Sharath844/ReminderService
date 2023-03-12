fbf2bdea9ae80e54f77544fb072cc06d516a608f
Lingabathula Sharath <sharathlingabathula@gmail.com>
2023-03-12 16:40:52 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {PORT} = require('./config/serverConfig');
+const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./service/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
 
-const setUpAndStartServer = () => {
+const jobs = require('./utils/job');
+
+const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-
-          sendBasicEmail(
-          'support@admin.com',
-          'backenddevelopment844@gmail.com',
-           'This is a testing email',
-         'Hey, how are you, I hope you like the support'
-        );
+        jobs();
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'moviebookingappservice@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // );
+              
     });
 }
-setUpAndStartServer();
+
+setupAndStartServer();
